818e7b13734f2461a895ce2dc09e9467d27a446e
DaliaZegaya <daliazegaya56@gmail.com>
2022-05-19 09:15:53 +0300

    commit

---
+++ main.js
@@ -0,0 +1,109 @@
+// function Eexercise34 () {
+//     var num= parseInt (+prompt("your number"))
+//     while (num!=0) {
+//         var result= num%10;
+//         console.log (result);
+//         num= (num- result) /10;
+//     }
+//     // var result= num%10
+//     // console.log (result)
+//     // num= num-result
+//     // num= num/10
+//     // result= num%10
+//     // console.log (result)
+//     // num= num- result
+//     // num= num/10
+//     // result= num%10
+//     // console.log (result)
+// }
+// Eexercise34 ()
+
+// var MyArray= [1, 2, 3, 4, 5, 6, "Dalia", "Or"]
+// for (i=0; i<MyArray.length; i++) {
+//     if (MyArray[i]*0!=0) {
+//         MyArray[i]= "not_number"
+
+//     }
+
+// }
+// console.log (MyArray)
+
+// function exercise14 (numbers) {
+//     var newArray= [];
+//     for (var i=0; i<numbers.length; i++) {
+//         if (numbers[i]>20) {
+//             newArray+=numbers[i]
+//         }
+//     }
+//     return console.log (newArray)
+
+// }
+// exercise14([5,3,25,85,1,95,4])
+
+// function exercise15 (myArray) {
+//     var newArray=[]
+//     for (var i=0; i<myArray.length; i++){
+//         newArray.push(i+ myArray[i])
+//     }
+//     return newArray
+// }
+// console.log (exercise15 (["one, two, three"]))
+
+
+// function getArrayFromIndex(array) {
+//     var array1=[]
+//     for(var i = 0; i<array.length;i++){
+//         array1.push(i+ ","+ array[i])
+//     }
+//     return array1
+// }
+// console.log(getArrayFromIndex([1,2,3])) 
+
+// function exercise16 (array16) {
+//     var newArray= [];
+//     for (var i=0; i<array16.length;i++) {
+//         if (array16[i]%2==0) {
+//             newArray+="even"
+//         }
+//         else newArray+=array16[i]
+//     }
+//     console.log (newArray)
+// }
+// exercise16 ([1,2,3,4,5,6])
+
+// function exercise17 (num1, num2) {
+//         if (num1+num2>=13) {
+//             console.log ("bar-mitzva")
+//         }
+//         else if (num1+num2==12) {
+//             console.log ("bat-mitzva")
+//         }
+//         else {
+//             console.log ("kid")
+//         }
+// }
+// exercise17(+prompt("number one"), +prompt("number two"))
+
+// function exercise19 (age, firstName, lastName, number) {
+//     for (var i=0; i<number; i++) {
+//         if (age>18) {
+//          console.log (firstName,lastName)
+//         }
+//         else{
+//             console.log (firstName,lastName)
+//             i=number
+//         }
+//     }
+    
+// } 
+
+// exercise19 (+prompt("your age"), prompt("first name"), prompt("last name"), +prompt("your number"))
+
+// function exercise20 () {
+//     for (var i=0; i<1000; i++) {
+//         console.log ("javascript is not JAVA")
+//     }
+// }
+// exercise20 ()
+
+
